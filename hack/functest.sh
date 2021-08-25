7fd5685ad2631044c12071a7d4cc5530b0ad3878
Or Shoval <oshoval@redhat.com>
2021-08-25 14:20:00 +0300

    kubectl, Support different cluster root directory
    In order to support CNAO testing of kubemacpool e2e,
    add support of KUBECTL env var, that allows using different cluster root directory,
    because when CNAO deploys the cluster, it is created on CNAO directory
    but the kmp tests are running from a temp kmp folder that
    doesn't have the cluster metadata.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- hack/functest.sh
+++ hack/functest.sh
@@ -5,4 +5,5 @@ set -xe
 source ./cluster/cluster.sh
 
 export KUBEVIRT_CLIENT_GO_SCHEME_REGISTRATION_VERSION=v1
+unset KUBECTL
 KUBECONFIG=${KUBECONFIG:-$(cluster::kubeconfig)} $GO test -test.timeout=1h -race -test.v ./tests/... $E2E_TEST_ARGS -ginkgo.v --test-suite-params="$POLARION_TEST_SUITE_PARAMS"
--- tests/kubectl/kubectl.go
+++ tests/kubectl/kubectl.go
@@ -2,16 +2,23 @@ package kubectl
 
 import (
 	"bytes"
+	"os"
 	"os/exec"
 )
 
 func Kubectl(command ...string) (string, string, error) {
 	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("./cluster/kubectl.sh", command...)
-	cmd.Dir = ".."
+	var cmd *exec.Cmd
+
+	kubectl, found := os.LookupEnv("KUBECTL")
+	if found {
+		cmd = exec.Command(kubectl, command...)
+	} else {
+		cmd = exec.Command("./cluster/kubectl.sh", command...)
+		cmd.Dir = ".."
+	}
 	cmd.Stderr = &stderr
 	cmd.Stdout = &stdout
 	err := cmd.Run()
 	return stdout.String(), stderr.String(), err
 }
-
